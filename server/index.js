de4981540f58f0144ed1bce947b1d46e49c9526c
nguyenthanhhoang21095 <thanhhoang021095.work@gmail.com>
2021-05-19 19:53:13 +0700

    change port 6060

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
-const port = 8000
+const port = 6060
 const dev = process.env.DEV_ENV === 'true'
 const app = next({ dev })
 const handler = app.getRequestHandler()
